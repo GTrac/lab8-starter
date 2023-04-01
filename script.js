3be873240721003591ce38644fb08fb61b6f2fbb
Gilbert Traczyk <traczyk.gilbert@gmail.com>
2023-03-31 21:18:02 -0400

    in progress

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 
 'use strict';
 
+const sections = Array.from(document.querySelectorAll('section'));
+
+for(var i = 0; i < sections.length; i++) {
+    sections[i].addEventListener("click", view);
+}
+
+function view(e) {
+    if(document.querySelector('.accordion-label.open')) {
+        let currentOpen = document.querySelector('.accordion-label.open');
+    }
+    let target = e.target;
+    target.classList.add('open');
+    currentOpen.classList.remove('open');
+}
